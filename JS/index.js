26a85607d439f0990b84e452e5bd9d9f14935a88
RhysFJohn <r.freeman-john@outlook.com>
2021-05-17 16:35:26 +0100

    added object functionality, not fully tested as of yet

--- JS/index.js
+++ JS/index.js
@@ -1,39 +1,50 @@
 let mxchar = 20;
 
-function ValidateTaskForm(){
-  let x = document.forms["myTaskForm"]["name"].value;
-  let y = document.forms["myTaskForm"]["description"].value;
-  let z = document.forms["myTaskForm"]["assignee"].value;
+let myTasks = [];
+let arrayback = localStorage.getItem("myTasksArray")
 
-  if (x = "" && x.length >= mxchar ){
-    alert("Name of Assigner must be filled out and below 20 characters");
+
+function ValidateName(){
+  let x = document.querySelector("#inputName").value;
+
+  if (x = "" && x.length >= mxchar){
+    alert("Name of Assigner must be filled and below 20 characters")
   }
+}
+
+function ValidateDesc(){
+  let y = document.querySelector("#inputDesc").value;
 
   if (y = "" && y.length >= mxchar){
-    alert("Description of task must be filled out and below 20 characters")
-  }
-
-  if (z = "" && z.length >= mxchar){
-    alert("Name of Assignee must be filled out and below 20 characters")
+    alert("Description must be filled and below 20 characters")
   }
 }
 
 document.querySelector("#myBtn").addEventListener('click', function(){
   console.log("button was clicked");
-  
-  let items = []
 
-  items.push(document.forms["myTaskForm"]["name"].value);
-  items.push(document.forms["myTaskForm"]["description"].value);
-  items.push(document.forms["myTaskForm"]["assignee"].value);
-  items.push(document.forms["myTaskForm"]["dueDate"].value);
-  items.push(document.forms["myTaskForm"]["status"].value);
+  let tname = document.forms["myTaskForm"]["taskname"].value;
+  let abname = document.forms["myTaskForm"]["name"].value;
+  let desc = document.forms["myTaskForm"]["description"].value;
+  let atname = document.forms["myTaskForm"]["assignee"].value;
+  let ddate = document.forms["myTaskForm"]["dueDate"].value;
+  let status = document.forms["myTaskForm"]["status"].value;
 
-  console.log(items)
+  console.log(tname,abname,desc,atname,ddate,status)
 })
+
+function storeTask(tname, abname, desc, atname, ddate, status){
+  let taskObject = {
+    "TaskName" : tname,
+    "AssignedBy" : abname,
+    "Description" : desc,
+    "AssignedTo" : atname,
+    "DueDate" : ddate,
+    "Status" : status
+  }
+}
 
 // function createCards(){
 
 // }
 
-ValidateTaskForm()
