cd664d5051cff2d50950f423e26f4c0232506786
RhysFJohn <r.freeman-john@outlook.com>
2021-05-17 14:27:15 +0100

    updated JavaScript with validation

--- JS/index.js
+++ JS/index.js
@@ -1,20 +1,20 @@
 let maxchar = 20;
 
 function ValidateTaskForm(){
-  let x = document.forms["myTaskForm"]["name"].value;
-  let y = document.forms["myTaskForm"]["description"].value;
-  let z = document.forms["myTaskForm"]["assignee"].value;
-  document.forms["myTaskForm"]["dueDate"].value;
+  // let x = document.forms["myTaskForm"]["name"].value;
+  // let y = document.forms["myTaskForm"]["description"].value;
+  // let z = document.forms["myTaskForm"]["assignee"].value;
+  // document.forms["myTaskForm"]["dueDate"].value;
 
-  if (x = "" && x.length >= maxchar ){
+  if (document.forms["myTaskForm"]["name"].value = "" && document.forms["myTaskForm"]["name"].value.length >= maxchar ){
     alert("Name of Assigner must be filled out and below 20 characters");
   }
 
-  if (y = "" && y.length >= maxchar){
+  if (document.forms["myTaskForm"]["description"].value = "" && document.forms["myTaskForm"]["description"].value.length >= maxchar){
     alert("Description of task must be filled out and below 20 characters")
   }
 
-  if (z = "" && z.length >=maxchar){
+  if (document.forms["myTaskForm"]["assignee"].value = "" && document.forms["myTaskForm"]["assignee"].value.length >= maxchar){
     alert("Name of Assignee must be filled out and below 20 characters")
   }
 }
