ba2fe0a22fc160caf87329d73aeef88aab08abb1
RhysFJohn <r.freeman-john@outlook.com>
2021-05-17 15:06:34 +0100

    This commit adds push functionality but doesn't have validation functionality for the form

--- JS/index.js
+++ JS/index.js
@@ -1,22 +1,39 @@
-let maxchar = 20;
+let mxchar = 20;
 
 function ValidateTaskForm(){
-  // let x = document.forms["myTaskForm"]["name"].value;
-  // let y = document.forms["myTaskForm"]["description"].value;
-  // let z = document.forms["myTaskForm"]["assignee"].value;
-  // document.forms["myTaskForm"]["dueDate"].value;
+  let x = document.forms["myTaskForm"]["name"].value;
+  let y = document.forms["myTaskForm"]["description"].value;
+  let z = document.forms["myTaskForm"]["assignee"].value;
 
-  if (document.forms["myTaskForm"]["name"].value = "" && document.forms["myTaskForm"]["name"].value.length >= maxchar ){
+  if (x = "" && x.length >= mxchar ){
     alert("Name of Assigner must be filled out and below 20 characters");
   }
 
-  if (document.forms["myTaskForm"]["description"].value = "" && document.forms["myTaskForm"]["description"].value.length >= maxchar){
+  if (y = "" && y.length >= mxchar){
     alert("Description of task must be filled out and below 20 characters")
   }
 
-  if (document.forms["myTaskForm"]["assignee"].value = "" && document.forms["myTaskForm"]["assignee"].value.length >= maxchar){
+  if (z = "" && z.length >= mxchar){
     alert("Name of Assignee must be filled out and below 20 characters")
   }
 }
 
-ValidateTaskForm();
+document.querySelector("#myBtn").addEventListener('click', function(){
+  console.log("button was clicked");
+  
+  let items = []
+
+  items.push(document.forms["myTaskForm"]["name"].value);
+  items.push(document.forms["myTaskForm"]["description"].value);
+  items.push(document.forms["myTaskForm"]["assignee"].value);
+  items.push(document.forms["myTaskForm"]["dueDate"].value);
+  items.push(document.forms["myTaskForm"]["status"].value);
+
+  console.log(items)
+})
+
+// function createCards(){
+
+// }
+
+ValidateTaskForm()
