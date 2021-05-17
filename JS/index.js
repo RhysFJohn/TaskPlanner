5ab3bad282b8a083df63ba91071cb910e0c23037
RhysFJohn <r.freeman-john@outlook.com>
2021-05-17 12:29:48 +0100

    Commit adds updated html with cards and an updated js file

---
+++ JS/index.js
@@ -0,0 +1,22 @@
+let maxchar = 20;
+
+function ValidateTaskForm(){
+  let x = document.forms["myTaskForm"]["name"].value;
+  let y = document.forms["myTaskForm"]["description"].value;
+  let z = document.forms["myTaskForm"]["assignee"].value;
+  let a = document.forms["myTaskForm"]["dueDate"].value;
+
+  if (x = "" && x.length >= maxchar ){
+    alert("Name of Assigner must be filled out and below 20 characters");
+  }
+
+  if (y = "" && y.length >= maxchar){
+    alert("Description of task must be filled out and below 20 characters")
+  }
+
+  if (z = "" && z.length >=maxchar){
+    alert("Name of Assignee must be filled out and below 20 characters")
+  }
+}
+
+ValidateTaskForm();
